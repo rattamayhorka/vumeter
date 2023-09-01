cea0583f285f1483719f326df0b270803440a5a7
rattamayhorka <raymundo.cadena@gmail.com>
2023-08-31 23:00:38 -0600

    se actualiza el software para un lcd de 4 lineas y se corrigen caracteres

--- main.c
+++ main.c
@@ -3,44 +3,31 @@
 #include <stdio.h>
 #include "lcd.h"
 
-const uint8_t customPatterns[9][8] = {
-
-
-    // Carácter 8
-    { 0b11111111, 0b11111111, 0b11111111, 0b11111111,
-      0b11111111, 0b11111111, 0b11111111, 0b11111111 },
-    
-// Carácter 1
-    { 0b00000000, 0b11111111, 0b11111111, 0b11111111,
-      0b11111111, 0b11111111, 0b11111111, 0b11111111 },
-
+const uint8_t customPatterns[8][8] = {
+    // Carácter 1
+    { 0b00000, 0b00000, 0b00000, 0b00000,
+      0b00000, 0b00000, 0b00000, 0b11111 },
     // Carácter 2
-    { 0b00000000, 0b00000000, 0b11111111, 0b11111111,
-      0b11111111, 0b11111111, 0b11111111, 0b11111111 },
-
+    { 0b00000, 0b00000, 0b00000, 0b00000,
+      0b00000, 0b00000, 0b11111, 0b11111 },
     // Carácter 3
-    { 0b00000000, 0b00000000, 0b00000000, 0b11111111,
-      0b11111111, 0b11111111, 0b11111111, 0b11111111 },
-
+    { 0b00000, 0b00000, 0b00000, 0b00000,
+      0b00000, 0b11111, 0b11111, 0b11111 },
     // Carácter 4
-    { 0b00000000, 0b00000000, 0b00000000, 0b00000000,
-      0b11111111, 0b11111111, 0b11111111, 0b11111111 },
-
+    { 0b00000, 0b00000, 0b00000, 0b00000,
+      0b11111, 0b11111, 0b11111, 0b111111 },
     // Carácter 5
-    { 0b00000000, 0b00000000, 0b00000000, 0b00000000,
-      0b00000000, 0b11111111, 0b11111111, 0b11111111 },
-
+    { 0b00000, 0b00000, 0b00000, 0b11111,
+      0b11111, 0b11111, 0b11111, 0b11111 },
     // Carácter 6
-    { 0b00000000, 0b00000000, 0b00000000, 0b00000000,
-      0b00000000, 0b000000000, 0b11111111, 0b11111111 },
-
+    { 0b00000, 0b00000, 0b11111, 0b11111,
+      0b11111, 0b11111, 0b11111, 0b11111 },
     // Carácter 7
-    { 0b00000000, 0b00000000, 0b00000000, 0b00000000,
-      0b00000000, 0b000000000, 0b00000000, 0b11111111 },
-
+    { 0b00000, 0b11111, 0b11111, 0b11111,
+      0b11111, 0b11111, 0b11111, 0b11111 },
     // Carácter 8
-    { 0b11111111, 0b11111111, 0b11111111, 0b11111111,
-      0b11111111, 0b11111111, 0b11111111, 0b11111111 },
+    { 0b11111, 0b11111, 0b11111, 0b11111,
+      0b11111, 0b11111, 0b11111, 0b11111 },
 };
 
 void adc_init() {
@@ -61,32 +48,29 @@ int main(void) {
     adc_init(); // Inicializar el ADC
     lcd_clrscr();
     // Cargar los patrones en la memoria de caracteres
-    lcd_command(0x40); // Iniciar en la dirección 0x40 (ubicación de caracteres especiales)
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 8; j++) {
-            lcd_data(customPatterns[i][j]);
-        }
+    for (int i = 0; i < 8; i++) {
+       lcd_command(0x40 + i * 8); // Moverse a la dirección de escritura correcta para el carácter i
+       for (int j = 0; j < 8; j++) {
+          lcd_data(customPatterns[i][j]); // Cargar el patrón en la memoria de caracteres
+       }
     }
-
     while (1) {
-        uint16_t adcValue = adc_read(); // Leer el valor del ADC en el pin 0 del puerto A (PA0)
-        //uint8_t charIndex = adcValue >> 5; // Calcular el índice para el carácter especial
-        if (adcValue < 512){
-           uint8_t charIndex = adcValue * 9 / 512; // Calcular el índice para el carácter especial
-              for (int col = 0; col < 16; col++) {
-                 lcd_gotoxy(col, 0); // Fila de arriba
-                 lcd_data(charIndex); // Mostrar el carácter especial en la posición actual
-              }
+        uint16_t adcValue = adc_read(); // Leer el valor del ADC
+        uint8_t charIndex = ((adcValue * 7) / 1023); // Calcular el índice para el carácter especial
+        for (int col = 0; col < 20; col++) {
+           lcd_gotoxy(col, 0); // Fila 1
+           lcd_data(charIndex); // Mostrar el carácter especial en la posición actual
+           lcd_gotoxy(col, 1); // Fila 2
+           lcd_data(charIndex); // Mostrar el carácter especial en la posición actual
+           lcd_gotoxy(col, 2); // Fila 3
+           lcd_data(charIndex); // Mostrar el carácter especial en la posición actual
+           lcd_gotoxy(col, 3); // Fila 4
+           lcd_data(charIndex); // Mostrar el carácter especial en la posición actual
         }
-        if (adcValue >= 512){
-           uint8_t charIndex = adcValue * 9 / 512; // Calcular el índice para el carácter especial
-              for (int col = 0; col < 16; col++) {
-                 lcd_gotoxy(col, 0); // Fila de arriba
-                 lcd_data(9); // Mostrar el carácter especial en la posición actual
-                 lcd_gotoxy(col, 1); // Fila de arriba
-                 lcd_data(charIndex); // Mostrar el carácter especial en la posición actual
-              }
+        if (charIndex > 7){
+           charIndex = 7;
         }
-        _delay_ms(100); // Esperar 200 ms antes de actualizar la pantalla
+
+    _delay_ms(100); // Esperar 100 ms antes de actualizar la pantalla
     }
 }
