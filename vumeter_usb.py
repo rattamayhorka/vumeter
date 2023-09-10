c2e45041df158120db7793132a88f3d14974c6b6
rattamayhorka <raymundo.cadena@gmail.com>
2023-09-10 10:51:44 -0600

    control analogico de volumen funcionando

--- vumeter_usb.py
+++ vumeter_usb.py
@@ -5,6 +5,8 @@ import subprocess
 ser = serial.Serial('/dev/ttyUSB0', 9600)  # Cambia a tu puerto serie y velocidad de baudios
 
 try:
+    char_buffer = ""  # Buffer para acumular caracteres
+
     while True:
         # Lee un byte desde el puerto serie
         byte = ser.read(1)
@@ -14,26 +16,31 @@ try:
 
         # Verifica si char_value es un dígito válido ('0'-'9')
         if char_value.isdigit():
-            # Convierte el carácter ASCII a un número entero
-            num_value = int(char_value)
-            
-            # Ajusta num_value al rango de 0 a 10
-            if num_value > 10:
-                num_value = 10
-
-            # Multiplica num_value por 10 para obtener el resultado en el rango de 0 a 100
-            result = num_value * 10
-
-            # Formatea result como una cadena con dos dígitos
-            result_str = "{:02d}".format(result)
-
-            # Muestra el resultado en la consola
-            print(result_str, end='')
-
-            # Ejecuta el comando pamixer para establecer el volumen
-            subprocess.run(["pamixer", "--set-volume", result_str])
+            # Acumula el carácter en el buffer
+            char_buffer += char_value
         else:
-            # Si no es un dígito, muestra el valor original
+            # Si no es un dígito, verifica si hay caracteres en el buffer
+            if char_buffer:
+                # Convierte el buffer en un número entero
+                num_value = int(char_buffer)
+                
+                # Ajusta num_value al rango de 0 a 99
+                if num_value > 99:
+                    num_value = 99
+
+                # Convierte num_value a cadena sin formateo adicional
+                result_str = str(num_value)
+
+                # Muestra el resultado en la consola
+                print(result_str, end='')
+
+                # Ejecuta el comando pamixer para establecer el volumen
+                subprocess.run(["pamixer", "--set-volume", result_str])
+
+                # Limpia el buffer
+                char_buffer = ""
+
+            # Muestra el valor original si no es un dígito
             print(char_value, end='')
 
 except KeyboardInterrupt:
