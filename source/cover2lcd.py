66321cfdbc0ef2bbe7a2cb0642bb2af91268a36a
rattamayhorka <raymundo.cadena@gmail.com>
2024-11-23 23:01:41 -0600

    manejo de imagen mpris sino encuentra en last.fm

--- source/cover2lcd.py
+++ source/cover2lcd.py
@@ -1,12 +1,11 @@
 import dbus
-import dbus.mainloop.glib
 import io
 from PIL import Image, ImageEnhance
-import subprocess
 import serial
 import time
 import requests
 import pyudev
+import subprocess
 import re
 
 # Función para leer la API key desde un archivo
@@ -29,6 +28,70 @@ def find_ttyusb_by_model(model_name):
     print(f"No se encontró el modelo: {model_name}")
     return None
 
+def rgb565(r, g, b):
+    return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
+
+def invert_color(r, g, b):
+    return (255 - r, 255 - g, 255 - b)
+
+def send_image_to_lcd(image, ser):
+    width, height = image.size
+    for y in range(height):
+        pixels = bytearray()
+        for x in range(width):
+            r, g, b = image.getpixel((x, y))
+            r, g, b = invert_color(r, g, b)
+            color = rgb565(r, g, b)
+            pixels.extend(color.to_bytes(2, 'big'))
+        ser.write(pixels)
+        time.sleep(0.1)
+
+def clean_title(title):
+    # Elimina partes comunes de los títulos
+    cleaned_title = re.sub(r'\s*\(.*?\)\s*', '', title)  # Eliminar texto entre paréntesis
+    cleaned_title = cleaned_title.strip()
+    return cleaned_title
+
+def search_album_art_on_lastfm(artist, title):
+    queries = [
+        f"https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={LASTFM_API_KEY}&artist={artist}&track={title}&format=json",
+        f"https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={LASTFM_API_KEY}&artist={artist}&track={clean_title(title)}&format=json"
+    ]
+
+    for query in queries:
+        response = requests.get(query)
+        if response.status_code == 200:
+            data = response.json()
+            if 'track' in data and 'album' in data['track']:
+                image_url = data['track']['album']['image'][-1]['#text']
+                if image_url:
+                    image_data = requests.get(image_url).content
+                    img = Image.open(io.BytesIO(image_data))
+                    img = img.convert("RGB").resize((240, 240))
+                    enhancer = ImageEnhance.Contrast(img)
+                    img = enhancer.enhance(1.5)
+                    return img
+            else:
+                print("No se encontró la pista con esa variante.")
+    print("No se encontró la portada en Last.fm.")
+    return None
+
+def get_metadata(player_name):
+    bus = dbus.SessionBus()
+    player = bus.get_object(player_name, '/org/mpris/MediaPlayer2')
+    properties = dbus.Interface(player, dbus_interface='org.freedesktop.DBus.Properties')
+    metadata = properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    artist = metadata.get('xesam:artist')[0] if 'xesam:artist' in metadata else None
+    title = metadata.get('xesam:title') if 'xesam:title' in metadata else None
+    album_art_url = metadata.get('mpris:artUrl') if 'mpris:artUrl' in metadata else None
+    return artist, title, album_art_url
+
+def get_active_player():
+    command = "busctl --user --list | grep org.mpris.MediaPlayer2 | head -n 1 | awk '{print $1}'"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return stdout.decode('utf-8').strip()
+
 # Buscar el puerto serie correspondiente al modelo especificado
 tty_lcd = find_ttyusb_by_model("CP2102_USB_to_UART_Bridge_Controller")
 
@@ -42,79 +105,32 @@ if tty_lcd is not None:
             print(f"Abriendo el puerto: {tty_lcd}")
             ser.open()
 
-        def rgb565(r, g, b):
-            return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
-
-        def invert_color(r, g, b):
-            return (255 - r, 255 - g, 255 - b)
-
-        def send_image_to_lcd(image):
-            width, height = image.size
-            for y in range(height):
-                pixels = bytearray()
-                for x in range(width):
-                    r, g, b = image.getpixel((x, y))
-                    r, g, b = invert_color(r, g, b)
-                    color = rgb565(r, g, b)
-                    pixels.extend(color.to_bytes(2, 'big'))
-                ser.write(pixels)
-                time.sleep(0.1)
-
-        def clean_title(title):
-            # Elimina partes comunes de los títulos de YouTube
-            cleaned_title = re.sub(r'\s*\(.*?\)\s*', '', title)  # Eliminar texto entre paréntesis
-            cleaned_title = cleaned_title.strip()
-            return cleaned_title
-
-        def search_album_art_on_lastfm(artist, title):
-            # Lista de consultas con el título original y el título limpio
-            queries = [
-                f"https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={LASTFM_API_KEY}&artist={artist}&track={title}&format=json",
-                f"https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={LASTFM_API_KEY}&artist={artist}&track={clean_title(title)}&format=json"
-            ]
-
-            for query in queries:
-                response = requests.get(query)
-                if response.status_code == 200:
-                    data = response.json()
-                    if 'track' in data and 'album' in data['track']:
-                        image_url = data['track']['album']['image'][-1]['#text']
-                        if image_url:
-                            image_data = requests.get(image_url).content
-                            img = Image.open(io.BytesIO(image_data))
-                            img = img.convert("RGB").resize((240, 240))
-                            enhancer = ImageEnhance.Contrast(img)
-                            img = enhancer.enhance(1.5)
-                            return img
-                    else:
-                        print("No se encontró la pista con esa variante.")
-            print("No se encontró la portada en Last.fm.")
-            return None
-
-        def get_metadata(player_name):
-            bus = dbus.SessionBus()
-            player = bus.get_object(player_name, '/org/mpris/MediaPlayer2')
-            properties = dbus.Interface(player, dbus_interface='org.freedesktop.DBus.Properties')
-            metadata = properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-            artist = metadata.get('xesam:artist')[0] if 'xesam:artist' in metadata else None
-            title = metadata.get('xesam:title') if 'xesam:title' in metadata else None
-            return artist, title
-
-        command = "busctl --user --list | grep org.mpris.MediaPlayer2 | head -n 1 | awk '{print $1}'"
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        player_name = stdout.decode('utf-8').strip()
+        player_name = get_active_player()
 
         if player_name:
-            artist, title = get_metadata(player_name)
+            artist, title, album_art_url = get_metadata(player_name)
+            album_art = None
+
             if artist and title:
-                current_album_art = search_album_art_on_lastfm(artist, title)
-                if current_album_art:
-                    print(f"inicio de envio de portada {artist} - {title}.")
-                    send_image_to_lcd(current_album_art)
-                    print(f"termino de envio de portada.")
+                album_art = search_album_art_on_lastfm(artist, title)
+
+            if not album_art and album_art_url:
+                if album_art_url.startswith("file://"):
+                    local_path = album_art_url.replace("file://", "")
+                    try:
+                        with open(local_path, "rb") as f:
+                            image_data = f.read()
+                        album_art = Image.open(io.BytesIO(image_data))
+                        album_art = album_art.convert("RGB").resize((240, 240))
+                    except FileNotFoundError:
+                        print(f"No se encontró el archivo local: {local_path}")
+
+            if album_art:
+                print(f"Iniciando envío de portada: {artist} - {title}.")
+                send_image_to_lcd(album_art, ser)
+                print("Envío de portada completado.")
             else:
-                print("No se pudo obtener el artista o el título.")
+                print("No se pudo obtener la portada.")
         else:
             print("No hay reproductores activos en este momento.")
 
